8c227d1ba0032257b492660132970ca97d20fd98
Jakob Skov-Pedersen <jasp@keepfocus.dk>
2010-08-05 14:39:57 +0200

    Historical parameter can be treated like attribute

--- lib/acts_as_historical_parameter.rb
+++ lib/acts_as_historical_parameter.rb
@@ -1,1 +1,20 @@
-# ActsAsHistoricalParameter
+module ActsAsHistoricalParameter
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def acts_as_historical_parameter(name, ident)
+      define_method(name) do
+        @historical_parameters ||= {}
+        @historical_parameters[name]
+      end
+      define_method("#{name}=") do |value|
+        @historical_parameters ||= {}
+        @historical_parameters[name] = value
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send :include, ActsAsHistoricalParameter
--- test/acts_as_historical_parameter_test.rb
+++ test/acts_as_historical_parameter_test.rb
@@ -1,17 +1,24 @@
 require 'test_helper'
 
+class Installation < ActiveRecord::Base
+  acts_as_historical_parameter :area, 1
+end
+
+class HistoricParameter < ActiveRecord::Base
+  belongs_to :parameterized, :polymorphic => true
+end
+
 class ActsAsHistoricalParameterTest < ActiveSupport::TestCase
   load_schema
-
-  class Installation < ActiveRecord::Base
-  end
-
-  class HistoricParameter < ActiveRecord::Base
-    belongs_to :parameterized, :polymorphic => true
-  end
 
   test "schema has loaded correctly" do
     assert_equal [], Installation.all
     assert_equal [], HistoricParameter.all
+  end
+
+  test "historic parameter works like regualar attribute" do
+    installation = Installation.new
+    installation.area = 42.0
+    assert_equal 42.0, installation.area
   end
 end
