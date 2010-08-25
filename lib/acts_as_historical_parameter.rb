e547720f16dd3a48560c106e41b8fd9d28f0838d
Jakob Skov-Pedersen <jasp@keepfocus.dk>
2010-08-25 13:54:11 +0200

    Extension for controllers to handle new value

--- lib/acts_as_historical_parameter.rb
+++ lib/acts_as_historical_parameter.rb
@@ -13,87 +13,102 @@ end
 require 'acts_as_historical_parameter/view_helper.rb'
 
 module ActsAsHistoricalParameter
-  def self.included(base)
-    base.extend(ClassMethods)
+  module ModelExtensions
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def define_historical_getter(name)
+        class_eval <<-EOM
+          def #{name}
+            @#{name} ||= #{name}_history.first :order => "valid_from DESC"
+            @#{name}.value if @#{name}
+          end
+        EOM
+      end
+
+      def define_historical_setter(name)
+        class_eval <<-EOM
+          def set_#{name}(value, from)
+            if value
+              @#{name} = #{name}_history.build :valid_from => from, :value => value
+            end
+          end
+          def #{name}=(value)
+            self.set_#{name}(value, Time.zone.now)
+          end
+        EOM
+      end
+
+      def define_historical_values(name)
+        class_eval <<-EOM
+          def #{name}_values
+            hps = #{name}_history.all(:order => "valid_from")
+            values = []
+            if hps.length >= 2
+              values = hps.each_cons(2).collect do |a|
+                [
+                  a[0].valid_from,
+                  a[1].valid_from,
+                  a[0].value
+                ]
+              end
+            end
+            lv = hps.last
+            if lv
+              values + [[lv.valid_from, nil, lv.value]]
+            else
+              nil
+            end
+          end      
+        EOM
+      end
+
+      def define_historical_sum(name)
+        class_eval <<-EOM
+          def #{name}_sum(start_time, end_time)
+            #{name}_values.sum do |entry|
+              if entry[1] and start_time < entry[1]
+                if start_time > entry[0]
+                  yield start_time, entry[1], entry[2]
+                else
+                  yield entry[0], entry[1], entry[2]
+                end
+              elsif entry[1].nil? and end_time > entry[0]
+                yield entry[0], end_time, entry[2]
+              else
+                0
+              end
+            end
+          end
+        EOM
+      end
+
+      def acts_as_historical_parameter(name, ident)
+        ass_sym = "#{name}_history".to_sym
+        has_many ass_sym, :as => :parameterized, :class_name => "HistoricalParameter", :conditions => {:ident => ident}
+        accepts_nested_attributes_for ass_sym, :allow_destroy => true
+        define_historical_getter(name)
+        define_historical_setter(name)
+        define_historical_values(name)
+        define_historical_sum(name)
+      end
+    end
   end
 
-  module ClassMethods
-    def define_historical_getter(name)
-      class_eval <<-EOM
-        def #{name}
-          @#{name} ||= #{name}_history.first :order => "valid_from DESC"
-          @#{name}.value if @#{name}
-        end
-      EOM
-    end
-
-    def define_historical_setter(name)
-      class_eval <<-EOM
-        def set_#{name}(value, from)
-          if value
-            @#{name} = #{name}_history.build :valid_from => from, :value => value
-          end
-        end
-        def #{name}=(value)
-          self.set_#{name}(value, Time.zone.now)
-        end
-      EOM
-    end
-
-    def define_historical_values(name)
-      class_eval <<-EOM
-        def #{name}_values
-          hps = #{name}_history.all(:order => "valid_from")
-          values = []
-          if hps.length >= 2
-            values = hps.each_cons(2).collect do |a|
-              [
-                a[0].valid_from,
-                a[1].valid_from,
-                a[0].value
-              ]
-            end
-          end
-          lv = hps.last
-          if lv
-            values + [[lv.valid_from, nil, lv.value]]
-          else
-            nil
-          end
-        end      
-      EOM
-    end
-
-    def define_historical_sum(name)
-      class_eval <<-EOM
-        def #{name}_sum(start_time, end_time)
-          #{name}_values.sum do |entry|
-            if entry[1] and start_time < entry[1]
-              if start_time > entry[0]
-                yield start_time, entry[1], entry[2]
-              else
-                yield entry[0], entry[1], entry[2]
-              end
-            elsif entry[1].nil? and end_time > entry[0]
-              yield entry[0], end_time, entry[2]
-            else
-              0
-            end
-          end
-        end
-      EOM
-    end
-
-    def acts_as_historical_parameter(name, ident)
-      ass_sym = "#{name}_history".to_sym
-      has_many ass_sym, :as => :parameterized, :class_name => "HistoricalParameter", :conditions => {:ident => ident}
-      accepts_nested_attributes_for ass_sym, :allow_destroy => true
-      define_historical_getter(name)
-      define_historical_setter(name)
-      define_historical_values(name)
-      define_historical_sum(name)
+  module ControllerExtensions
+    def handle_add_value(object, method, attributes)
+      if params[:"add_#{method}_value"]
+        object.attributes = attributes
+        object.send(method).build(:valid_from => Time.now)
+        render :action => "edit"
+      else
+        false
+      end
     end
   end
 end
 
-ActiveRecord::Base.send :include, ActsAsHistoricalParameter
+ActiveRecord::Base.send :include, ActsAsHistoricalParameter::ModelExtensions
+ActionController::Base.send :include, ActsAsHistoricalParameter::ControllerExtensions
---
+++ test/test_historical_controller_addons.rb
@@ -0,0 +1,59 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+class DummyInstallationsController < ActionController::Base
+  def rescue_action(e)
+    raise e
+  end
+
+  def show
+  end
+
+  def update
+    @installation = Installation.find(params[:id])
+    return if handle_add_value(@installation, :area_history, params[:installation])
+
+    respond_to do |format|
+      if @installation.update_attributes(params[:installation])
+        format.html { redirect_to(dummy_installation_path(@installation)) }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
+  end
+end
+
+class TestHistoricalControllerAddons < ActionController::TestCase
+  tests DummyInstallationsController
+
+  def setup
+    ActionController::Routing::Routes.draw {|map| map.resources :dummy_installations }
+    @routes = Rails.application.routes
+  end
+
+  test "Update works for value add" do
+    @installation = Installation.create(:name => "original")
+      
+    put :update, :id => @installation.to_param, :add_area_history_value => "test", :installation => {
+      :name => "changed"
+    }
+    assert_response :success
+    assert_not_nil assigns(:installation)
+    assert assigns(:installation).area_history.last.new_record?
+    assert_equal "changed", assigns(:installation).name
+    assert_equal "original", Installation.find(@installation.to_param).name
+  end
+
+  test "Update works for save new state" do
+    @installation = Installation.create(:name => "original")
+    put :update, :id => @installation.to_param, :installation => {
+      :area_history_attributes => [
+        {:value => 42, :valid_from => Time.zone.local(2010, 1, 1)},
+        {:value => 42, :valid_from => Time.zone.local(2010, 2, 1)}
+      ],
+      :name => "changed"
+    }
+    assert_redirected_to dummy_installation_path(@installation)
+    assert_equal "changed", Installation.find(@installation.to_param).name
+    assert_equal 42, Installation.find(@installation.to_param).area
+  end
+end
